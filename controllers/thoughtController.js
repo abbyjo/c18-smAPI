8dfd0b96d463c4ced626129ae0acf9aa26c79a71
abbyjo <abbyjo225@gmail.com>
2023-11-04 19:23:15 -0400

    Added initial structure of thought controller functions

---
+++ controllers/thoughtController.js
@@ -0,0 +1,33 @@
+const User = require('../models/User');
+const Thoughts = require('../models/Thoughts');
+
+async function getThoughts (req, res){}
+
+
+async function oneThought (req, res){}
+
+
+async function createThought (req, res){}
+
+
+async function updateThought (req, res){}
+
+
+async function deleteThought (req, res){}
+
+
+async function createReaction (req, res){}
+
+
+async function removeReaction (req, res){}
+
+
+module.exports = {
+    getThoughts,
+    oneThought,
+    createThought,
+    updateThought,
+    deleteThought,
+    createReaction,
+    removeReaction
+}
